84635a3264f75ac7805d2f5e5442b852de8e08f2
hzcaohuanhuan <hzcaohuanhuan@corp.netease.com>
2017-01-09 16:53:25 +0800

    remove unuse

--- static/lib/qpf/build/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-node update.js
-node r.js -o config.js
-lessc ../src/style/base.less ../dist/css/base.css
-cp -r ../src/style/images ../dist/css/images
--- static/lib/qpf/build/qpf_template.js
+++
@@ -1,8 +0,0 @@
-define(function(require){
-    
-    var qpf =  {{$exportsObject}};
-
-    qpf.create = qpf.Base.create;
-
-    return qpf;
-})
